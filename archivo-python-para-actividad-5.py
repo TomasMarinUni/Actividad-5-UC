aa1f64f4c817e38b846b4fbb43c4426bc620ec6c
Tomas <tomasmarin@estudiante.uc.cl>
2025-05-12 05:52:25 -0400

    archivo python añadido

---
+++ archivo-python-para-actividad-5.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+while True:
+    print("Tengo 18 horas para entregar el trabajo y ni idea lo que estoy haciendo")
+print(f'Sum of {a} and {b} is {sum(a, b)}')
